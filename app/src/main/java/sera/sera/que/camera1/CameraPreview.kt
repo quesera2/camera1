eeb6ba8ec235622044fcfd0ad3bc079c3c4e379e
quesera2 <glayash2484@gmail.com>
2018-12-15 16:52:54 +0900

    add handle runtime permission

--- app/src/main/java/sera/sera/que/camera1/CameraPreview.kt
+++ app/src/main/java/sera/sera/que/camera1/CameraPreview.kt
@@ -1,21 +1,80 @@
 package sera.sera.que.camera1
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Camera
 import android.util.AttributeSet
+import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.widget.FrameLayout
+import androidx.annotation.RequiresPermission
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 
+@Suppress("deprecation")
 class CameraPreview @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0
-) : FrameLayout(context, attrs, defStyle) {
+) : FrameLayout(context, attrs, defStyle), LifecycleObserver {
 
-    private val surfaceView: SurfaceView = SurfaceView(context).apply {
-        holder.addCallback(Callback())
-        addView(this)
+    private val tag = "CameraPreview"
+
+    private val surfaceView: SurfaceView = SurfaceView(context)
+
+    private var camera: Camera? = null
+
+    init {
+        surfaceView.holder.addCallback(Callback())
+        addView(surfaceView)
     }
+
+    @RequiresPermission(Manifest.permission.CAMERA)
+    fun open() {
+        Log.i(tag, "open camera")
+        if (camera == null) {
+            val backCameraId = findBackCameraId()
+            camera = Camera.open(backCameraId)
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    private fun startPreview() {
+        Log.i(tag, "start preview")
+
+        val permission = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+        if (camera == null
+            && permission == PackageManager.PERMISSION_GRANTED
+        ) {
+            open()
+        }
+
+        // TODO
+        camera?.let { camera ->
+            camera.setPreviewDisplay(surfaceView.holder)
+            camera.startPreview()
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    private fun stopPreview() {
+        Log.i(tag, "stop preview")
+
+        // TODO
+        camera?.stopPreview()
+        camera?.release()
+        camera = null
+    }
+
+    private fun findBackCameraId(): Int = (0..Camera.getNumberOfCameras())
+        .asSequence()
+        .map { cameraId -> cameraId to Camera.CameraInfo().also { Camera.getCameraInfo(cameraId, it) } }
+        .first { it.second.facing == Camera.CameraInfo.CAMERA_FACING_BACK }
+        .first
 
     private inner class Callback : SurfaceHolder.Callback {
         override fun surfaceCreated(surface: SurfaceHolder?) {
--- app/src/main/java/sera/sera/que/camera1/MainActivity.kt
+++ app/src/main/java/sera/sera/que/camera1/MainActivity.kt
@@ -1,16 +1,48 @@
 package sera.sera.que.camera1
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var preview: CameraPreview
+
+    private val requestCamera = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         preview = findViewById(R.id.preview)
+        lifecycle.addObserver(preview)
+
+        checkPermission()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == requestCamera) {
+            assert(permissions.first() == Manifest.permission.CAMERA)
+            if (grantResults.first() == PackageManager.PERMISSION_GRANTED) {
+                startCamera()
+            }
+        }
+    }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), requestCamera)
+        }
+    }
+
+    private fun startCamera() {
+        assert(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+        preview.open()
     }
 }
